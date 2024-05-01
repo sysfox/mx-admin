859d7cdf7390236b8a3d9b4558b3892f0dc6a73b
Innei <Innei@users.noreply.github.com>
2024-05-01 14:19:43 +0000

    deploy: a4a37062e9b78fdd670207657bececa01a0feeb0

---
+++ js/two-col-DDvafx9b.js
@@ -0,0 +1,1 @@
+import{d as o,e as t,bH as a}from"./index-CtTzMi8p.js";const d=o({setup(r,{slots:e}){return()=>t(a,{cols:"36 1:12 1024:36 1600:36",xGap:24},{default:()=>[e?.default?.()]})}});export{d as T};
