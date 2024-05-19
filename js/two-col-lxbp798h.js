b08902fac1e843a9a9c934f2e071befb8428d85a
Innei <Innei@users.noreply.github.com>
2024-05-19 12:38:37 +0000

    deploy: f49ced78049a61e19bb15be6c8821755aea13b4f

---
+++ js/two-col-lxbp798h.js
@@ -0,0 +1,1 @@
+import{d as o,e as t,bG as a}from"./index-BaYnj12Y.js";const d=o({setup(r,{slots:e}){return()=>t(a,{cols:"36 1:12 1024:36 1600:36",xGap:24},{default:()=>[e?.default?.()]})}});export{d as T};
